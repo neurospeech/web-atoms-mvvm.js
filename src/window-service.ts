81616e1e0277be04357733357ce0e2c713c366aa
Akash Kava <support@neurospeech.com>
2017-11-23 14:16:56 +0530

    wip

--- src/window-service.ts
+++ src/window-service.ts
@@ -10,6 +10,93 @@ namespace WebAtoms {
     export class WindowService {
 
         /**
+         * Reference used by popup opener as an anchor
+         * @type {HTMLElement}
+         * @memberof WindowService
+         */
+        currentTarget:HTMLElement;
+
+        popups:AtomControl[] = [];
+
+        /**
+         *
+         */
+        constructor() {
+			window.addEventListener("click", (e) => {
+                this.currentTarget = e.target as HTMLElement;
+                this.closePopup();
+			});
+        }
+
+        private closePopup():void {
+            if(!this.popups.length) {
+                return;
+            }
+            var peek:AtomControl = this.popups[this.popups.length-1];
+            var element:HTMLElement = peek._element;
+
+            while(element) {
+                if(element === this.currentTarget) {
+                    // close this popup....
+                    return;
+                }
+                element = element.parentElement;
+            }
+        }
+
+        lastPopupID:number = 0;
+
+        public openAsync<T>(p: any, vm: AtomWindowViewModel ): Promise<T> {
+            return new Promise((resolve,reject) => {
+
+                var parent:AtomControl = Core.atomParent(this.currentTarget);
+
+                var e:HTMLDivElement = document.createElement("div");
+                // tslint:disable-next-line:no-string-literal
+                e["_logicalParent"] = parent._element;
+
+                e.id = `atom_popup_${this.lastPopupID++}`;
+
+                if(vm) {
+                    // tslint:disable-next-line:no-string-literal
+                    vm["windowName"] = e.id;
+                }
+
+                var r:Rect = Core.getOffsetRect(this.currentTarget);
+
+                e.style.position = "absolute";
+                e.style.left = r.x + "px";
+                e.style.top = (r.y + r.height) + "px";
+
+                document.body.appendChild(e);
+                var ct:AtomControl = new p(e);
+                ct.viewModel = vm;
+                ct.createChildren();
+                ct.init();
+
+                var d:{ close?: AtomDisposable, cancel?: AtomDisposable } = {};
+
+                d.close = AtomDevice.instance.subscribe(`atom-window-close:${e.id}`,
+                (g,i) => {
+                    ct.dispose();
+                    e.remove();
+                    d.close.dispose();
+                    d.cancel.dispose();
+                    resolve(i);
+                });
+
+                d.cancel = AtomDevice.instance.subscribe(`atom-window-cancel:${e.id}`,
+                    (g,i)=> {
+                    ct.dispose();
+                    e.remove();
+                    d.close.dispose();
+                    d.cancel.dispose();
+                    reject(i);
+                });
+            });
+        }
+
+        /**
          * Resolves current Window Service, you can use this method
          * to resolve service using DI, internally it calls
          * DI.resolve(WindowService).
